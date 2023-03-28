cba6092ceb69ddab76cc8d5731649252d1e7b764
OnlyHalfEvil333 <95574693+OnlyHalfEvil333@users.noreply.github.com>
2023-03-28 13:23:05 +0600

    j

--- src/main/java/entity/Staff.java
+++ src/main/java/entity/Staff.java
@@ -1,2 +1,27 @@
-package entity;public class Staff {
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Component
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Scope("prototype")
+@Entity(name = "staff")
+@Table
+public class Staff {
+
+    @Id
+    @Column(name = "ID")
+    private int staff_id;
+
 }
