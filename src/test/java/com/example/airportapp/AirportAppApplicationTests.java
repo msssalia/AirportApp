ec9187a64d45a907dbd1e87ccc761757b23b837a
OnlyHalfEvil333 <95574693+OnlyHalfEvil333@users.noreply.github.com>
2023-03-28 13:05:08 +0600

    Initial commit

---
+++ src/test/java/com/example/airportapp/AirportAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airportapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
